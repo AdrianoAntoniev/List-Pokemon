e649039df6d2a0622193eabb5c5e737453ef0a9d
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-09-29 16:56:35 -0300

    Initial Commit

---
+++ List-Pokemon!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List-Pokemon!
+//
+//  Created by Adriano Rodrigues Vieira on 29/09/20.
+//  Copyright © 2020 Adriano Rodrigues Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
